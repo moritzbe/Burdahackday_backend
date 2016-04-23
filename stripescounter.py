e0606fab29ba2b2062ee5dde32e963cee0e01640
¨budickda¨ <daniel@budick.eu>
2016-04-23 15:53:02 +0200

    image opened in OpenCV morphologic functions

--- stripescounter.py
+++ stripescounter.py
@@ -18,12 +18,10 @@ from PIL import Image
 #Load dummy-json-file
 with open('image.json') as dataFile:
   data = json.load(dataFile)
-
 #get base64 string of image
 base64ImageString = re.sub('^data:image/.+;base64,', '', data['src']).decode('base64')
 image = Image.open(BytesIO(base64ImageString))
 image.thumbnail((image.size[0]/5,image.size[1]/5,), Image.ANTIALIAS)
-
 im = np.array(image.getdata()).reshape(image.size[1], image.size[0], 3)
 	
 # load tree image
@@ -74,4 +72,35 @@ plt.imshow(whiteout);plt.title('Red');
 # plt.subplot(2,2,4)
 # plt.imshow(filtered, cmap='gray');plt.title('filtered_red');
 
-plt.show()
+#optimized image
+optimizedImage = whiteout
+optimizedImage = np.uint8(optimizedImage*255)
+
+openCvImage = cv2.cvtColor(optimizedImage, cv2.COLOR_RGB2BGR)
+
+kernel = np.ones((2,2),np.uint8)
+for i in range(5):
+	openCvImage = cv2.erode(openCvImage,kernel,iterations = 1)
+	openCvImage = cv2.dilate(openCvImage,kernel,iterations = 1)
+
+
+# contours, _ = cv2.findContours(openCvImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+# for c in contours:
+# 	rect = cv2.boundingRect(c)
+# 	if rect[2] < 5 or rect[3] < 5: continue
+# 	print cv2.contourArea(c)
+#Show image
+cv2.imshow('image',openCvImage)
+
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+
+#contours, _ = cv2.findContours(openCvImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+#for c in contours:
+#    rect = cv2.boundingRect(c)
+#    if rect[2] < 5 or rect[3] < 5: continue
+#    print cv2.contourArea(c)
+
+
+#plt.show()
